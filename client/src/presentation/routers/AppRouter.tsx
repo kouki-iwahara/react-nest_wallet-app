dc927d3b63eba7db11d63b4bceaf3962bade7db1
岩原公貴 <iwaharakouki@iwaharakoutakanoMacBook-Pro.local>
2020-06-24 22:52:33 +0900

    コンポーネント名の修正

--- client/src/presentation/routers/AppRouter.tsx
+++ client/src/presentation/routers/AppRouter.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Resister from '../pages/signup/Signup'
+import Signup from '../pages/signup/Signup'
 import Login from '../pages/login/Login'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const AppRouter = () => (
   <Router>
     <Route exact path="/" component={Login}></Route>
-    <Route exact path="/signup" component={Resister}></Route>
+    <Route exact path="/signup" component={Signup}></Route>
   </Router>
 )
 
