f1b0acd35edfe82402de68f34223115300baea4c
岩原公貴 <iwaharakouki@iwaharakoutakanoMacBook-Pro.local>
2020-06-24 22:47:12 +0900

    不要なコード削除

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,8 @@
 import React, { useState } from 'react'
 import './App.css'
-import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState('')
-  const fetchTest = async () => {
-    const test = await axios.get('/test').then((res) => res.data)
-    setData(test.data)
-  }
-  fetchTest()
-  return (
-    <div className="App">
-      <div>{data}</div>
-      <div>{data}</div>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
