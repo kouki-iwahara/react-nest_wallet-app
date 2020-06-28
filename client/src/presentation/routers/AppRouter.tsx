9d6bf5e053a01f0e6df6853e2415763f068a0363
岩原公貴 <iwaharakouki@iwaharakoutakanoMacBook-Pro.local>
2020-06-28 15:48:01 +0900

    route設定

--- client/src/presentation/routers/AppRouter.tsx
+++ client/src/presentation/routers/AppRouter.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Signup from '../pages/signup/Signup'
-import Login from '../pages/login/Login'
+import SignupContainer from '../pages/signup/Signup.container'
+import Signin from '../pages/signin/Signin'
+import Dashboard from '../pages/mypage/Dashboard'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const AppRouter = () => (
   <Router>
-    <Route exact path="/" component={Login}></Route>
-    <Route exact path="/signup" component={Signup}></Route>
+    <Route exact path="/" component={Signin}></Route>
+    <Route exact path="/signup" component={SignupContainer}></Route>
+    <Route exact path="/mypage/dashboard" component={Dashboard}></Route>
   </Router>
 )
 
